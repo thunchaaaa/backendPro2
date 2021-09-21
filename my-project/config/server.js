eebda500c74c656651566f23fe008e76f610556f
thunchaaaaa <thunchanok_du@cmu.ac.th>
2021-09-22 01:07:34 +0700

    finish
    finish

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c953d95923abfa9e6e51a7966dc92519'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a7234cba-3be9-4011-b3cc-4ea2193641ee'
+};
